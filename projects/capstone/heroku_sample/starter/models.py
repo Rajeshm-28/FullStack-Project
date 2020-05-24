c3f4d7dc3046b534cfdf30bf93c6817d3f2c2fd8
Rajeshm-28 <rajeshm.28@gmail.com>
2020-05-24 22:45:25 +0530

    initial

--- projects/capstone/heroku_sample/starter/models.py
+++ projects/capstone/heroku_sample/starter/models.py
@@ -1,8 +1,10 @@
-from sqlalchemy import Column, String, create_engine
+import os
+from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_path = os.environ['DATABASE_URL']
+database_path = "postgresql://DELL@localhost:5432/heroku_sample"
+
 
 db = SQLAlchemy()
 
